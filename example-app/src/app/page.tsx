ede3813b3979534be11a1cd1588c3bb740122ccf
Stefan Loeschcke <stefanbugge@gmail.com>
2024-05-10 15:24:29 +0200

    wip

---
+++ example-app/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+import sdf from "@weld/integration-logos";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+  );
+}
