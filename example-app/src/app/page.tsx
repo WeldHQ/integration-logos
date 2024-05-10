207549a993fc527649fdc9b649e61d6f17863676
Stefan Loeschcke <stefanbugge@gmail.com>
2024-05-10 15:51:59 +0200

    wip

--- example-app/src/app/page.tsx
+++ example-app/src/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
 
-// import sdf from "@weld/integration-logos";
+// import config from "@weld/integration-logos/";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      {/* <Image src={config} alt="ActiveCampaign Logo" /> */}
+    </main>
   );
 }
