71e71c299c90d22954ea99af995e542b10a95872
Stefan Loeschcke <stefanbugge@gmail.com>
2024-05-10 23:41:39 +0200

    wip

--- scripts/flat-copy-image-files.ts
+++ scripts/flat-copy-image-files.ts
@@ -6,6 +6,10 @@ import config from "../config.json";
 const imagesFolder = path.join(process.cwd(), process.argv[2]);
 const distFolder = path.join(process.cwd(), process.argv[3]);
 
+if (!fs.existsSync(distFolder)) {
+  fs.mkdirSync(distFolder, { recursive: true });
+}
+
 Object.entries(config).forEach(([integrationId, integrationConfig]) => {
   fs.copyFile(
     path.join(imagesFolder, integrationId, integrationConfig.fileName),
